95022ade203960fd4435b53d627e0ee411b3b4f0
Matthew Metcalfe <mjmxanomaly@gmail.com>
2019-11-23 21:30:19 -0800

    Task Api + Swagger

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './tasks.models';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.taskService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body('title') title: string, @Body('description') description: string): Task {
+    return this.taskService.createTask(title, description);
+  }
+}
